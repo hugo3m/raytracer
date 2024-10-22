b1fc1c0c2637c87a7493eef97dd73dd6cb384e7c
Hugo Martineau <hugo.martineau3@gmail.com>
2024-10-22 12:41:31 +0200

    add pkg

---
+++ pkg/wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} width
+* @param {number} height
+* @returns {Uint8Array}
+*/
+export function draw(width: number, height: number): Uint8Array;

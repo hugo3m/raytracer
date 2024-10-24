fcafd79a1aa919e88ce4d38aca5aa5788ce3ec56
Hugo Martineau <hugo.martineau3@gmail.com>
2024-10-24 15:52:29 +0200

    try again on prod?

--- components/main.tsx
+++ components/main.tsx
@@ -4,26 +4,8 @@ import Canvas from "./canvas";
 
 export default function Test() {
 
-  // const [canvasProps, setCanvasProps] = useState<Nullable<CanvasProps>>(null);
-
-  // const updateCanvasProps = () => {
-  //   if (window !== undefined){
-  //     const gap: number = 50;
-  //     const maxSize: number = 600;
-  //     const limit: number = Math.min(window.innerWidth, window.innerHeight);
-  //     const limitIntToString: string = limit.toFixed(0).toString();
-  //     const power: number = limitIntToString.length;
-  //     const exponent: number = parseInt(limitIntToString[0]);
-  //     const size: number = (exponent * (10 ** (power - 1))) - gap;
-  //     setCanvasProps({width: Math.min(size, maxSize), height: Math.min(size, maxSize)});
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   updateCanvasProps();
-  // }, [])
-
   return (
     <div className="flex flex-1 justify-center p-1"><Canvas width={600} height={600}/></div>
   );
+
 }

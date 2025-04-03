b763a93bfb46aae4ad39e6d1397f9d083fc50e16
hugo martineau <hugo.martineau@sqhead.com>
2025-04-03 11:26:13 +0200

    add phone lay

--- components/main.tsx
+++ components/main.tsx
@@ -38,82 +38,167 @@ export default function Main() {
       }
   }, [sphereNumber, pixels, isDiffuse, isSpecular, isShadow, isReflective, cameraSpeed]);
 
-  return (
-  <div className="flex flex-1 flex-col">
-    <div className="flex flex-row justify-center">
-      <div className="flex flex-1 flex-col p-12">
-          <span>Presentation:</span>
-          <p>
-              Homemade raytracer made by <a className="text-blue-700" href="https://github.com/hugo3m">hugo3m</a> inspired from
-              <a className="text-blue-700" href="https://gabrielgambetta.com/computer-graphics-from-scratch/index.html"> Computer graphics from scratch, Gabriel Gambetta</a>.
-                Built using Rust web assembly and NextJS. You can find the repository at <a className="text-blue-700" href="https://github.com/hugo3m/raytracer">hugo3m/raytracer</a>.
-          </p>
-      </div>
-      <div className="flex flex-2 flex-col max-w-[30vw] min-w-[30vw]">
-        <span className="">{fps.toFixed(0)}FPS</span>
-        <canvas ref={canvasRef} width={pixels} height={pixels}/>
-          {inputInfo ? <div className="flex flex-col items-center">
-            <div className="flex flex-row">
-                <Input input="Q" isDown={inputInfo.up}/>
-                <Input input="W" isDown={inputInfo.forward}/>
-                <Input input="E" isDown={inputInfo.down}/>
-            </div>
-            <div className="flex flex-row">
-                <Input input="A" isDown={inputInfo.left}/>
-                <Input input="S" isDown={inputInfo.backward}/>
-                <Input input="D" isDown={inputInfo.right}/>
-            </div>
-            </div> : null}
-      </div>
+  const renderLaptop = () => {
+    return (
+      <div className="flex flex-1 flex-col">
+      <div className="flex flex-raw justify-center">
         <div className="flex flex-1 flex-col p-12">
-            <div>
-              <span>Parameters:</span>
-              <div>
-              <FormGroup>
-                <FormControlLabel control={<Checkbox checked={isDiffuse} onChange={(event) => setIsDiffuse(event.target.checked)} />} label="Diffuse light" />
-                <FormControlLabel control={<Checkbox checked={isSpecular} onChange={(event) => setIsSpecular(event.target.checked)}/>} label="Specular light" />
-                <FormControlLabel control={<Checkbox checked={isReflective} onChange={(event) => setIsReflection(event.target.checked)} />} label="Reflection" />
-                <FormControlLabel control={<Checkbox checked={isShadow} onChange={(event) => setIsShadow(event.target.checked)} />} label="Shadow" />
-              </FormGroup>
+            <span>Presentation:</span>
+            <p>
+                Homemade raytracer made by <a className="text-blue-700" href="https://github.com/hugo3m">hugo3m</a> inspired from
+                <a className="text-blue-700" href="https://gabrielgambetta.com/computer-graphics-from-scratch/index.html"> Computer graphics from scratch, Gabriel Gambetta</a>.
+                  Built using Rust web assembly and NextJS. You can find the repository at <a className="text-blue-700" href="https://github.com/hugo3m/raytracer">hugo3m/raytracer</a>.
+            </p>
+        </div>
+        <div className="flex flex-2 flex-col items-center">
+          <span className="">{fps.toFixed(0)}FPS</span>
+          <canvas className="max-w-[35vw] min-w-[35vw]" ref={canvasRef} width={pixels} height={pixels}/>
+            {inputInfo ? <div className="flex flex-col items-center">
+              <div className="flex flex-row">
+                  <Input input="Q" isDown={inputInfo.up}/>
+                  <Input input="W" isDown={inputInfo.forward}/>
+                  <Input input="E" isDown={inputInfo.down}/>
               </div>
+              <div className="flex flex-row">
+                  <Input input="A" isDown={inputInfo.left}/>
+                  <Input input="S" isDown={inputInfo.backward}/>
+                  <Input input="D" isDown={inputInfo.right}/>
+              </div>
+              </div> : null}
+        </div>
+          <div className="flex flex-1 flex-col p-12">
               <div>
-                  <span>Camera speed</span>
+                <span>Parameters:</span>
+                <div>
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox checked={isDiffuse} onChange={(event) => setIsDiffuse(event.target.checked)} />} label="Diffuse light" />
+                  <FormControlLabel control={<Checkbox checked={isSpecular} onChange={(event) => setIsSpecular(event.target.checked)}/>} label="Specular light" />
+                  <FormControlLabel control={<Checkbox checked={isReflective} onChange={(event) => setIsReflection(event.target.checked)} />} label="Reflection" />
+                  <FormControlLabel control={<Checkbox checked={isShadow} onChange={(event) => setIsShadow(event.target.checked)} />} label="Shadow" />
+                </FormGroup>
+                </div>
+                <div>
+                    <span>Camera speed</span>
+                    <Slider
+                        value={cameraSpeed}
+                        onChange={(event, value) => setCameraSpeed(value as number)}
+                        marks
+                        valueLabelDisplay="auto"
+                        min={2}
+                        step={1}
+                        max={10}
+                    />
+                </div>
+                <div>
+                    <span>Number of pixels width and height</span>
+                    <Slider
+                        value={pixels}
+                        onChange={(event, value) => setPixels(value as number)}
+                        marks
+                        valueLabelDisplay="auto"
+                        min={100}
+                        step={100}
+                        max={1000}
+                    />
+                </div>
+                <div>
+                  <span>Number of spheres</span>
                   <Slider
-                      value={cameraSpeed}
-                      onChange={(event, value) => setCameraSpeed(value as number)}
+                      value={sphereNumber}
+                      onChange={(event, value) => setSphereNumber(value as number)}
                       marks
                       valueLabelDisplay="auto"
-                      min={2}
-                      step={1}
+                      min={1}
                       max={10}
                   />
+                </div>
               </div>
-              <div>
-                  <span>Number of pixels width and height</span>
-                  <Slider
-                      value={pixels}
-                      onChange={(event, value) => setPixels(value as number)}
-                      marks
-                      valueLabelDisplay="auto"
-                      min={100}
-                      step={100}
-                      max={1000}
-                  />
-              </div>
-              <div>
-                <span>Number of spheres</span>
-                <Slider
-                    value={sphereNumber}
-                    onChange={(event, value) => setSphereNumber(value as number)}
-                    marks
-                    valueLabelDisplay="auto"
-                    min={1}
-                    max={10}
-                />
-              </div>
-            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
+
+  const renderPhone = () => {
+    return (
+      <div className="flex flex-1 flex-col">
+      <div className="flex flex-col justify-center">
+        <div className="flex flex-1 flex-col p-12">
+            <span>Presentation:</span>
+            <p>
+                Homemade raytracer made by <a className="text-blue-700" href="https://github.com/hugo3m">hugo3m</a> inspired from
+                <a className="text-blue-700" href="https://gabrielgambetta.com/computer-graphics-from-scratch/index.html"> Computer graphics from scratch, Gabriel Gambetta</a>.
+                  Built using Rust web assembly and NextJS. You can find the repository at <a className="text-blue-700" href="https://github.com/hugo3m/raytracer">hugo3m/raytracer</a>.
+            </p>
+        </div>
+        <div className="flex flex-2 flex-col items-center">
+          <span className="">{fps.toFixed(0)}FPS</span>
+          <canvas className="max-w-[70vw] min-w-[70vw]" ref={canvasRef} width={pixels} height={pixels}/>
+            {inputInfo ? <div className="flex flex-col items-center">
+              <div className="flex flex-row">
+                  <Input input="Q" isDown={inputInfo.up}/>
+                  <Input input="W" isDown={inputInfo.forward}/>
+                  <Input input="E" isDown={inputInfo.down}/>
+              </div>
+              <div className="flex flex-row">
+                  <Input input="A" isDown={inputInfo.left}/>
+                  <Input input="S" isDown={inputInfo.backward}/>
+                  <Input input="D" isDown={inputInfo.right}/>
+              </div>
+              </div> : null}
+        </div>
+          <div className="flex flex-1 flex-col p-12">
+              <div>
+                <span>Parameters:</span>
+                <div>
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox checked={isDiffuse} onChange={(event) => setIsDiffuse(event.target.checked)} />} label="Diffuse light" />
+                  <FormControlLabel control={<Checkbox checked={isSpecular} onChange={(event) => setIsSpecular(event.target.checked)}/>} label="Specular light" />
+                  <FormControlLabel control={<Checkbox checked={isReflective} onChange={(event) => setIsReflection(event.target.checked)} />} label="Reflection" />
+                  <FormControlLabel control={<Checkbox checked={isShadow} onChange={(event) => setIsShadow(event.target.checked)} />} label="Shadow" />
+                </FormGroup>
+                </div>
+                <div>
+                    <span>Camera speed</span>
+                    <Slider
+                        value={cameraSpeed}
+                        onChange={(event, value) => setCameraSpeed(value as number)}
+                        marks
+                        valueLabelDisplay="auto"
+                        min={2}
+                        step={1}
+                        max={10}
+                    />
+                </div>
+                <div>
+                    <span>Number of pixels width and height</span>
+                    <Slider
+                        value={pixels}
+                        onChange={(event, value) => setPixels(value as number)}
+                        marks
+                        valueLabelDisplay="auto"
+                        min={100}
+                        step={100}
+                        max={1000}
+                    />
+                </div>
+                <div>
+                  <span>Number of spheres</span>
+                  <Slider
+                      value={sphereNumber}
+                      onChange={(event, value) => setSphereNumber(value as number)}
+                      marks
+                      valueLabelDisplay="auto"
+                      min={1}
+                      max={10}
+                  />
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  return (typeof window !== 'undefined' && window.innerWidth <= 600) ?  renderPhone() : renderLaptop();
 }
